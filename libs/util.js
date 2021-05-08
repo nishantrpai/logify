2d64ddeabfa09a6be6787bd3afe97788bf21b92b
Nishant Pai <eiglesias914@gmail.com>
2021-05-08 11:28:39 +0530

    removed clipboard item

--- libs/util.js
+++ libs/util.js
@@ -1,6 +1,5 @@
 export const copyText = (text) => {
-    var data = [new ClipboardItem({ "text/plain": new Blob([text], { type: "text/plain" }) })];
-    navigator.clipboard.write(data).then(function () {
+    navigator.clipboard.writeText(text).then(function () {
         console.log("Copied to clipboard successfully!");
     }, function () {
         console.error("Unable to write to clipboard. :-(");
