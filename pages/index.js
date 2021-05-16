dde1ed4120a1d04285b6b2d5404eaaf19050b84e
botvader <eiglesias914@gmail.com>
2021-05-16 18:07:11 +0530

    added dark mode

--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,8 @@ const EventLog = () => {
 
   return (
     <div className="flex flex-col flex-wrap max-w-lg items-center justify-around w-full mt-6">
-      <div className={`p-6 border bg-white mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isFocused ? "shadow shadow-xl border-gray-50" : "border-gray-200"}`}>
-        <h3 className="text-2xl font-bold">Event Log</h3>
+      <div className={`p-6 border bg-white dark:bg-gray-900 mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isFocused ? "shadow shadow-xl border-gray-50" : "border-gray-200"} dark:border-none`}>
+        <h3 className="text-2xl font-bold dark:text-gray-300">Event Log</h3>
         <span className="mt-2 mb-2 text-gray-500 flex text-sm">Preview:</span>
         <span className="mt-1 max-w-sm text-gray-400 text-sm">
           {`${timeNow(date)}:`}&nbsp;
@@ -43,7 +43,7 @@ const EventLog = () => {
         </span>
         <input
           type="text"
-          class="px-2 py-3 leading-5 border w-full mt-4 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+          class="px-2 py-3 leading-5 border dark:bg-gray-700 dark:text-gray-50 dark:border-none w-full mt-4 rounded-md focus:outline-none focus:ring-2 focus:border-blue-500"
           placeholder="Enter Event"
           onFocus={() => { setIsFocused(true) }}
           onBlur={() => { setIsFocused(false) }}
@@ -100,8 +100,8 @@ const IO = () => {
 
   return (
     <div className="flex flex-col flex-wrap max-w-lg items-center justify-around w-full mt-6">
-      <div className={`p-6 border bg-white mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isFocused ? "shadow shadow-xl border-gray-100" : "border-gray-200"}`}>
-        <h3 className="text-2xl font-bold">I/O Log</h3>
+      <div className={`p-6 border bg-white dark:bg-gray-900 mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isFocused ? "shadow shadow-xl border-gray-50" : "border-gray-200"} dark:border-none`}>
+        <h3 className="text-2xl font-bold dark:text-gray-300">I/O Log</h3>
         <p className="mt-2 mb-2 text-gray-500 flex text-sm">Preview:</p>
         <p className="mt-1 mb-2 max-w-md text-gray-400 text-sm flex">{`${getDataEntry(
           dataType,
@@ -111,8 +111,11 @@ const IO = () => {
           <div className="relative inline-block text-left w-1/3">
             <button
               className="inline-flex  w-full px-4 py-2 text-sm font-medium 
-            text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 
-            focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+            text-gray-700 transition duration-150 ease-in-out bg-white 
+            dark:bg-gray-700 dark:border-none border dark:text-gray-300 
+            border-gray-300 rounded-md hover:text-gray-500 dark:hover:text-gray-50 
+            focus:outline-none focus:border-blue-300 focus:shadow-outline-blue 
+            dark:active:bg-gray-800 active:bg-gray-50 active:text-gray-800"
               onClick={() => {
                 setVisibility(!isVisible);
               }}
@@ -129,11 +132,11 @@ const IO = () => {
 
             <div
               className={`${isVisible ? "block" : "hidden"
-                } absolute bg-white border visible w-full`}
+                } absolute bg-white border visible w-full dark:bg-gray-800 dark:border-none`}
             >
               <div className="py-1">
                 <span
-                  className="text-gray-700 flex w-full px-4 py-2 text-sm leading-5 text-left"
+                  className="text-gray-700 dark:text-gray-300 flex w-full px-4 py-2 text-sm leading-5 text-left"
                   onClick={() => {
                     setDataType("Markdown");
                     setVisibility(false);
@@ -142,7 +145,7 @@ const IO = () => {
                   Markdown
                 </span>
                 <span
-                  className="text-gray-700 flex w-full px-4 py-2 text-sm leading-5 text-left"
+                  className="text-gray-700 dark:text-gray-300 flex w-full px-4 py-2 text-sm leading-5 text-left"
                   onClick={() => {
                     setDataType("CSV");
                     setVisibility(false);
@@ -157,7 +160,7 @@ const IO = () => {
 
         <input
           type="text"
-          class="px-2 py-3 leading-5 border w-full mt-4  rounded-md focus:outline-none focus:ring focus:border-blue-400"
+          class="px-2 py-3 leading-5 border dark:bg-gray-700 dark:text-gray-50 dark:border-none w-full mt-4 rounded-md focus:outline-none focus:ring-2 focus:border-blue-500"
           placeholder="Enter ',' separated input, output values"
           onFocus={() => { setIsFocused(true) }}
           onBlur={() => { setIsFocused(false) }}
@@ -207,8 +210,8 @@ const TimeStamp = () => {
 
   return (
     <div className="flex flex-col flex-wrap max-w-lg items-center justify-around w-full mt-6">
-      <div className={`p-6 border bg-white mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isCopied ? "shadow shadow-xl border-gray-50" : "border-gray-200"}`}>
-        <h3 className="text-2xl font-bold">Timestamp</h3>
+      <div className={`p-6 border bg-white dark:bg-gray-900 mt-6 text-left w-full rounded-xl transition delay-100 ease-in ${isCopied ? "shadow shadow-xl border-gray-50" : "border-gray-200"} dark:border-none`}>
+        <h3 className="text-2xl font-bold dark:text-gray-300">Timestamp</h3>
         <span className="mt-2 mb-2 text-gray-500 flex text-sm">Preview:</span>
         <span className="mt-1 max-w-sm text-gray-400 text-sm">
           Timestamp:&nbsp;
@@ -235,7 +238,7 @@ export default function Home() {
   const [alert, setalert] = useState("success");
 
   return (
-    <div className="flex bg-white flex-col items-center justify-start min-h-screen py-2">
+    <div className="flex bg-white dark:bg-gray-800 flex-col items-center justify-start min-h-screen py-2">
       <Head>
         <title>Logify — Preview, Edit and Generate Event Logs</title>
         <meta name="title" content="Logify — Preview, Edit and Generate Event Logs" />
@@ -276,7 +279,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center max-w-2xl justify-start flex-1 px-10 py-20 text-center">
-        <h1 className="text-6xl mb-4 font-bold">Logify</h1>
+        <h1 className="text-6xl mb-4 font-bold dark:text-gray-100">Logify</h1>
         <p className="mt-6 flex text-gray-500">
           ✍️ Record events, inputs, and outputs in your logfile
         </p>
@@ -285,7 +288,7 @@ export default function Home() {
         <TimeStamp />
       </main>
 
-      <footer className="flex flex-col items-center w-full justify-start flex-1 px-10 py-10 text-center border-t">
+      <footer className="flex flex-col items-center w-full justify-start flex-1 px-10 py-10 text-center border-t dark:border-none">
 
         <a
           className="flex justify-center text-blue-500 mb-8"
